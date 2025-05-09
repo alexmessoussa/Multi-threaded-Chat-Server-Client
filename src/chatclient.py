56651a43cc2739c4595b05112f5a29edf1b153f6
alexm <alexmessoussa5@gmail.com>
2025-05-10 03:15:27 +1000

    fix: send protocol add: message broadcast

--- src/chatclient.py
+++ src/chatclient.py
@@ -14,32 +14,29 @@ from socket import AF_INET, SOCK_STREAM, socket
 @dataclass
 class ChatClient:
     socket: socket = field(init=False)
+    name: str = argv[2]
     
     def __post_init__(self):
         port = int(argv[1])
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect(('localhost', port))
         
-        self.socket.send(argv[2].encode())
+        self.socket.send(self.name.encode())
         
         receive_thread = Thread(target=self.receive_handler, daemon=True)
         
         receive_thread.start()
-        
-
+    
     def interact(self):
         while True:
-            message = input().encode()
-            self.send(message)
-           
+            message = input().strip()
+            event = MessageEvent(name=self.name, message=message)
+            self.send(event)
             
-    def send(self,message:bytes):
-
-        self.socket.send(message)
-        self.socket.send(b'\x00')
-            
+    def send(self,event:Event):
+        message = _Event.serialise(event)
+        self.socket.send(message + b'\n')            
             #probably a match check to see what stuff it wants to send. ie. a message or a command and send that to a method that makes an Event and sends it?
-    
     
     def receive_handler(self):
         buffer = b''
@@ -48,13 +45,17 @@ class ChatClient:
             if not data:
                 break
             buffer += data
-            while b'\x00' in buffer:
-                message, buffer = buffer.split(b'\x00', 1)
+            while b'\n' in buffer:
+                message, buffer = buffer.split(b'\n', 1)
                 self.receive(message)
-        
     
     def receive(self, message:bytes):
-        print(message.decode())
+        event = _Event.deserialise(message)
+        
+        match event:
+            case MessageEvent(name = n, message = m):
+                print(f"[{n}] {m}")
+    
     
     
 client = ChatClient()
